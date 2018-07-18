d92bda7a26e522b0ad3fbd62db7f8cbe59c88c2d
Stephen Laing <laingste000@alpine.local>
2018-07-18 08:45:23 -0600

    functional button components and styles

---
+++ src/components/app.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Picker from './picker';
+import Button from './button';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="grid">
+        <h1 className="grid__title">Birthday Countdown</h1>
+
+        <div className="grid__skew-dark-one-box"></div>
+        <div className="grid__skew-dark-two"></div>
+        <div className="grid__skew-dark-three"></div>
+
+        <div className="grid__skew-light-one"></div>
+        <div className="grid__skew-light-two"></div>
+        <div className="grid__skew-light-three-box"></div>
+
+        <Picker/>
+        { Button('Generate Countdown') }
+      </div>
+    );
+  }
+}
---
+++ src/components/picker.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+import DatePicker from "react-datepicker";
+import moment from "moment";
+
+import 'react-datepicker/dist/react-datepicker.css';
+
+class Picker extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: moment()
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(date) {
+    console.log('trying to change date for', date._d);
+    this.setState({
+      startDate: date
+    });
+  }
+
+  render() {
+    return (
+      <div className="picker">
+        <DatePicker
+          selected={this.state.startDate}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+
+}
+
+export default Picker;
