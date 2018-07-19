53a342eb4afd5ba78e10f1f5f9a1fff52fe94eda
Stephen Laing <laingste000@alpine.local>
2018-07-19 12:00:31 -0600

    added age calculation

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+
 import Picker from "./picker";
 import Button from "./button";
 import Clock from "./clock";
 import ChangeDate from "./changeDate";
 import LargeText from "./largeText";
+
 import moment from "moment";
 
 export default class App extends Component {
@@ -20,7 +22,8 @@ export default class App extends Component {
         hours: 0,
         minutes: 0,
         seconds: 0
-      }
+      },
+      age: 0
     };
 
     this.handleGenerate = this.handleGenerate.bind(this);
@@ -28,22 +31,31 @@ export default class App extends Component {
 
   handleChange = function(date) {
     console.log("APP JS HANDLE CHANGE", date._d);
-    clearInterval(this.timer);
+    clearInterval(this.timer)
     this.setState({
       startDate: date
     });
   }.bind(this);
 
   handleGenerate = function() {
-    this.setState({ active: true });
-
+    
+   
     var bday = this.state.startDate.toDate();
     var today = new Date();
     var currentMonth = today.getMonth();
     var birthMonth = bday.getMonth();
 
+     
+    var timeBetween = today.getTime() - bday.getTime();
+    var daysOld = Math.floor(timeBetween / (1000 * 60 * 60 * 24))
+    var age = Number((daysOld/365).toFixed(0));
+    this.setState({ 
+      age,
+      active: true
+    })
+
     if(birthMonth > currentMonth) {
-      bday.setFullYear(today.getFullYear());
+      bday.setFullYear(today.getFullYear())
     } else if(birthMonth < currentMonth) {
       bday.setFullYear(today.getFullYear() + 1)
     } else if(birthMonth == currentMonth) {
@@ -59,9 +71,10 @@ export default class App extends Component {
     }
 
     var countDownDate = bday.getTime();
-    this.timer = setInterval(function() {
-      var now = today.getTime();
 
+    this.timer = setInterval(function() {
+
+      var now = today.getTime();
       var distance = countDownDate - now;
 
       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -94,7 +107,7 @@ export default class App extends Component {
         ChangeDate("Change Date", () => this.setState({ active: false })),
         LargeText("04/03"),
         <label className="grid__remaining">
-          Remaining until your 21st birthday
+          Remaining until you turn {this.state.age + 1}
         </label>
       ];
     } else {
